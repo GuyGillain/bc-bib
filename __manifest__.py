d15144cec5f8946a90eacd1eab0b8b7b7f2b8f66
GuyGillain <115338293+GuyGillain@users.noreply.github.com>
2022-12-10 09:57:18 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Module de gestion d/'un catalogue de livres",
+    "category": "Stock",
+    "Version": "15.0.0",
+    "author": "BURNIAUX CONSULTING",
+    "website": "",
+    "depends": ["base"],
+    "date":
+        [
+            'views/book.xml',
+            'security/ir.model.access.csv',
+        ],
+    "installable": True,
+
+}
