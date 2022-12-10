be924875fdc3e214bb897244e3f7f7f822363c21
GuyGillain <115338293+GuyGillain@users.noreply.github.com>
2022-12-10 11:09:59 +0100

    Update de books

--- models/book.py
+++ models/book.py
@@ -4,4 +4,58 @@ class book(models.Model):
 	_name = "book.model"
 	_description = "Model concernant les livres"
 
-	#nom_column = fields.Type()
+	fields_Title = fields.char(
+		string = "Titre",
+		require = "True",
+		help = "Le titre du livre",
+		index = "True"
+	)
+
+	fields_Screen_cover = fields.Binary(
+		string = "Photo de la couverture",
+		require = "True",
+		attachment = "True"
+	)
+
+	fields_Publishing_date = fields.Date(
+		string = "Date de publication",
+		require = "True"
+	)
+
+	fields_Editing = fields.Selection(
+		string = "Edition",
+		selection = [
+			('1','1ere'), ('2','2eme'), ('3','3eme'), ('4','4eme'), ('5','Xeme')
+		],
+		require = "True",
+		help = "Faite le choix de l\'édition"
+	)
+
+	fields_Page_count = fields.Integer(
+		string ="Nombre de pages",
+		require = "True"
+	)
+
+	fields_Bar_code = fields.Integer(
+		string = "code Barrrrreeee",
+		require = "True"
+	)
+
+	fields_ISBN = fields.Char(
+		string = "Code ISBN",
+		require = "True",
+		size = 50
+	)
+
+	fields_Format = fields.Selection(
+		string="Format",
+		require="True",
+		selection=[
+			('1', 'Papier'), ('2', 'Numérique')
+		]
+	)
+
+	fields_Pice = fields.Float(
+		string="Prixxxxxx",
+		require="True"
+	)
