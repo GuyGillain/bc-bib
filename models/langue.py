a0fe3160ee616d287a6b4514ba0ba7cde09b5457
Etienne12345678910 <varlet.etienne321@gmail.com>
2023-01-14 10:59:39 +0100

    modif

--- models/langue.py
+++ models/langue.py
@@ -2,7 +2,8 @@ from odoo import fields, models
 
 class langue(models.Model) :
      _name = "bib.langue"
-     _description= "langue"
+     _description= "langue",
+     _rec_name = "Fields_langue"
 
      Fields_langue = fields.Char(
 
