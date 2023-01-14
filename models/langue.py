eb38efa8ba37a008b7be1e088d4e4467bdb6b410
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2023-01-14 11:03:24 +0100

    Correction model Langue

--- models/langue.py
+++ models/langue.py
@@ -2,12 +2,10 @@ from odoo import fields, models
 
 class langue(models.Model) :
      _name = "bib.langue"
-     _description= "langue",
+     _description= "langue"
      _rec_name = "Fields_langue"
 
      Fields_langue = fields.Char(
-
         string="langue",
         require="True"
-
      )
