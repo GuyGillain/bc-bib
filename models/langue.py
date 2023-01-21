e87bc902807fa413ddbb67b2c3652c2783ec259b
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2023-01-21 10:25:51 +0100

    update

--- models/langue.py
+++ models/langue.py
@@ -1,6 +1,12 @@
 from odoo import fields, models
 
 class langue(models.Model) :
+   
+
+   def _count_number_book(self):
+      for rec in self:
+         rec.count_book = len(rec.ids_book)
+
      _name = "bib.langue"
      _description= "langue"
      _rec_name = "Fields_langue"
@@ -13,6 +19,11 @@ class langue(models.Model) :
      ids_book = fields.One2many(
       comodel_name="bib.book",
       inverse_name="langue_id",
-      string="ID des livres"
+      string="Nombre"
+      )
+
+     count_book = fields.Integer(
+      string="Nombre de bouquins",
+      compute=_count_number_book
       )
 
