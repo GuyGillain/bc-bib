16b001bd13cd6f13279301b944b55b0fe6b0dfb3
Delire Stéphane <116341760+stephane-delire@users.noreply.github.com>
2023-01-28 10:51:43 +0100

    Update evaluation.py

--- models/evaluation.py
+++ models/evaluation.py
@@ -4,10 +4,11 @@ class evaluation(models.Model):
 
 	_name = "bib.evaluation"
 	_description = "Evaluation des boukins"
+	_rec_name = "note"
 
 	note = fields.Selection(
 		string="Note",
-		selection=[("1", "1/5"), ("2", "2/5"), ("3", "3/5"), ("4", "4/5"), ("5", "5/5")],
+		selection=[("1/5", "1/5"), ("2/5", "2/5"), ("3/5", "3/5"), ("4/5", "4/5"), ("5/5", "5/5")],
 		required="True"
 	)
 	appreciation = fields.Char(
